55577c7e5ba92941d9ccad58c39b3610d63b29e5
Nick Freear <nfreear@yahoo.co.uk>
2026-04-19 12:23:03 +0100

    Add README to `@nfreear/vtt-data` repository (#2)
    * https://github.com/nfreear/audio-describe/issues/2

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Vendor VTT
+
+Demonstration WebVTT tracks that are copyright © to third-party publishers.
+
+Used in:
+
+* Demo: [nfreear.github.io/audio-describe/demo][ghp]
+* Code: [github.com/nfreear/audio-describe][code]
+
+[ghp]: https://nfreear.github.io/audio-describe/demo/
+[code]: https://github.com/nfreear/audio-describe.git
+[vtt-data]: https://github.com/nfreear/vtt-data.git
--- index.js
+++ index.js
@@ -4,15 +4,19 @@
 const DATA = [
   {
     id: 'yt:vbde2',
-    title: 'Christmas at …',
-    rights: '© All rights reserved.',
+    title: 'Christmas at a Downton Abbey Filming Location',
+    rights: '© VisitBritain 2026. All rights reserved.',
     mediaUrl: 'https://www.youtube.com/watch?v=HgHj3g68Tr4',
     trackUrl: import.meta.resolve('./tracks/visit-britain-downton-v2.ext-ad.en.vtt'),
+    duration: '01:39',
     provider: 'youtube'
   }, {
     id: 'yt:vbdex',
+    title: 'Christmas at …',
+    rights: '© VisitBritain 2026. All rights reserved.',
     mediaUrl: 'https://www.youtube.com/watch?v=HgHj3g68Tr4',
     trackUrl: import.meta.resolve('../tracks/visit-britain-downton.ext-ad.en.vtt'),
+    duration: '01:39',
     provider: 'youtube'
   }
 ];
