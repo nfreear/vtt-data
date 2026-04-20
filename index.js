eb17c5d62bafd8c12b6044f7944492844afe8789
Nick Freear <nfreear@yahoo.co.uk>
2026-04-20 20:15:45 +0100

    Adding "Mux: pouring coffee into a cup"

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ const DATA = [
     title: 'Christmas at …',
     rights: '© VisitBritain 2026. All rights reserved.',
     mediaUrl: 'https://www.youtube.com/watch?v=HgHj3g68Tr4',
-    trackUrl: import.meta.resolve('../tracks/visit-britain-downton.ext-ad.en.vtt'),
+    trackUrl: import.meta.resolve('./tracks/visit-britain-downton.ext-ad.en.vtt'),
     duration: '01:39',
     provider: 'youtube'
+  }, {
+    id: 'mx:cof',
+    title: 'Mux: Pouring coffee into a cup.',
+    rights: 'unknown',
+    mediaUrl: 'https://stream.mux.com/OfjbQ3esQifgboENTs4oDXslCP5sSnst/high.mp4',
+    trackUrl: import.meta.resolve('./tracks/mux-pour-coffee.ext-ad.en.vtt'),
+    duration: '01:41',
+    provider: 'video'
   }
 ];
 
